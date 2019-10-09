2d18db173b5b26601da972a8600f04456761aeac
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2019-10-09 11:52:52 +0100

    Correcting vscode

--- src/Processor/LogProcessorService.cs
+++ src/Processor/LogProcessorService.cs
@@ -12,7 +12,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Formatting;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
